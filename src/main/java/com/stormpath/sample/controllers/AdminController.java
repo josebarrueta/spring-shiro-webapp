4af4d13bccad0bf891ba957ca872a94a674836d3
josebarrueta <jose@stormpath.com>
2013-01-22 23:51:44 -0800

    Adding authorization support, externalizing properties file. Improving README file.

---
+++ src/main/java/com/stormpath/sample/controllers/AdminController.java
@@ -0,0 +1,11 @@
+package com.stormpath.sample.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbarrueta
+ * Date: 1/22/13
+ * Time: 11:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdminController {
+}
